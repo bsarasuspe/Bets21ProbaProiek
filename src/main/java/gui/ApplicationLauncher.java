4a4c6ef64aa60be497946d2849c7714f919add55
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2021-09-22 11:42:44 +0200

    adsfasd

--- src/main/java/gui/ApplicationLauncher.java
+++ src/main/java/gui/ApplicationLauncher.java
@@ -28,7 +28,7 @@ public class ApplicationLauncher {
 		System.out.println("Locale: "+Locale.getDefault());
 		
 		
-// Kaixo Nikolas
+// Kaixo Nikolas a
 		try {
 			
 			BLFacade appFacadeInterface;
